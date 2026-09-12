4f9b97c94b790db77c58cdf76f96fca5c1291e11
aniruddha <aniruddha.kajale7@gmail.com>
2026-09-12 12:45:53 +0530

    implemented the login for Person class

--- src/com/sunbeam/day6/inheritance/Person.java
+++ src/com/sunbeam/day6/inheritance/Person.java
@@ -1,5 +1,27 @@
 package com.sunbeam.day6.inheritance;
 
+import java.util.Scanner;
+
 public class Person {
-    
+
+    private String name;
+    private String mobileNo;
+
+    public Person(){
+        System.out.println("Person Class ctor");
+    }
+
+//    Accept the person data from user
+    public void acceptPerson(Scanner sc){
+        System.out.println("Enter name: ");
+        name = sc.next();
+        System.out.println("Enter mobile no: ");
+        mobileNo = sc.next();
+    }
+
+//    display person
+    public void displayPerson(){
+        System.out.println("name: "+name);
+        System.out.println("Mobile: "+mobileNo);
+    }
 }
